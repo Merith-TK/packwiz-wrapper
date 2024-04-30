23295725a57a475a7f1210d30fc9bd74dd17b04d
Merith-TK <merith.tk@gmail.com>
2024-04-30 14:59:31 -0700

    complete rework of import internals

--- cmd/pw/import.go
+++ cmd/pw/import.go
@@ -39,8 +39,43 @@ func importcmd(args []string) {
 		importFromFile(filename, autoConfirm)
 		return
 	}
-	importFromString(strings.Join(args, ","), autoConfirm)
+	// import from string
+	fmt.Println("[PackWrap] [NOTICE] importing from string is experimental and may not work as expected.")
+	importFromString(args, autoConfirm)
 
+}
+
+// returns url, path, name
+func parseLine(line string, previousLine string) (string, string, string) {
+	if !strings.Contains(line, " ") {
+		// get filename from url
+		filename := strings.Split(line, "/")[len(strings.Split(line, "/"))-1]
+		return line, previousLine, filename
+	}
+	parts := strings.Split(line, " ")
+	// https://cdn.merith.xyz/icon.png /images/
+	// return https://cdn.merith.xyz/images.png, /images/, icon.png
+	if strings.HasSuffix(previousLine, "/") {
+		url := parts[0]
+		path := parts[1]
+		name := strings.Split(url, "/")[len(strings.Split(url, "/"))-1]
+		return url, path, name
+	}
+	// https://cdn.merith.xyz/icon.png /icon.png
+	// return https://cdn.merith.xyz/images.png, /, icon.png
+	url := parts[0]
+	path := parts[1]
+	pathParts := strings.Split(path, "/")
+	if len(pathParts) > 1 {
+		path = strings.Join(pathParts[:len(pathParts)-1], "/") + "/"
+	}
+	if path == "" {
+		path = "/"
+	}
+	// get name from end of path
+	name := pathParts[len(pathParts)-1]
+	fmt.Println("[PackWrap] [NOTICE] `packwiz` does not support importing file urls to specific filenames yet.\n\tusing the filename from the url.")
+	return url, path, name
 }
 
 func importFromFile(importFile string, autoConfirm bool) {
@@ -55,82 +90,93 @@ func importFromFile(importFile string, autoConfirm bool) {
 	data := strings.Split(fileContent, "\n")
 	previousLine := ""
 	for _, line := range data {
-		line = strings.ReplaceAll(line, " ", "")
-		line = strings.ReplaceAll(line, "	", "")
+		// clean formatting to make it easier to parse
+		line = strings.TrimLeft(line, " ")
+		line = strings.TrimLeft(line, "\t")
+		// skip lines that are not urls or directories
 		if !strings.HasPrefix(line, "https://") {
 			if strings.HasPrefix(line, "/") {
 				previousLine = line
 			}
 			continue
 		} else {
-			installArgs := []string{}
+			if importFromSource(line, autoConfirm) {
+				continue
+			}
+			if importFromURL(line, previousLine, autoConfirm) {
+				continue
+			}
 
-			// strip URL into base parts
-			urlPrefix := ""
-			if strings.HasPrefix(line, "https://") {
-				urlPrefix = "https://"
-				line = strings.ReplaceAll(line, "https://", "")
-			} else if strings.HasPrefix(line, "http://") {
-				urlPrefix = "http://"
-				line = strings.ReplaceAll(line, "http://", "")
-			}
-			if strings.HasPrefix(line, "www.") {
-				line = strings.ReplaceAll(line, "www.", "")
-			}
-			urlParts := strings.Split(line, "/")
-			line = urlPrefix + strings.Join(urlParts, "/")
-			switch {
-			case urlParts[0] == "curseforge.com" && urlParts[1] == "minecraft" && urlParts[2] == "mc-mods":
-				installArgs = append(installArgs, "cf", "add", line)
-			case urlParts[0] == "modrinth.com" && !strings.HasSuffix(urlParts[1], "s") && len(urlParts) > 2:
-				installArgs = append(installArgs, "mr", "add", line)
-			default:
-				installArgs = append(installArgs, "url", "add")
-				filepath := strings.Split(previousLine, "/") // split the url by /
-				filename := filepath[len(filepath)-1]        // get the last element of the url
-				filename = strings.Split(filename, ".")[0]   // remove the file extensions
-				installArgs = append(installArgs, filename)
-				installArgs = append(installArgs, line)
-			}
-			packwiz(*flagPackDir, installArgs)
-			previousLine = ""
 		}
 	}
 }
 
-func importFromString(importString string, autoConfirm bool) {
-	data := strings.Split(importString, " ")
-	for _, line := range data {
-		line = strings.ReplaceAll(line, " ", "")
-		line = strings.ReplaceAll(line, "	", "")
-		if !strings.HasPrefix(line, "https://") {
-			continue
-		} else {
-			installArgs := []string{}
+func importFromString(mods []string, autoConfirm bool) {
+	source := mods[0]
+	// verify source
+	if source != "cf" && source != "mr" && source != "url" {
+		fmt.Println("[PackWrap] [ERROR] invalid source")
+		return
+	}
+	if source != "url" {
+		for _, mod := range mods[1:] {
 			if autoConfirm {
-				installArgs = append(installArgs, "-y")
+				packwiz(*flagPackDir, []string{source, "add", mod, "-y"})
+			} else {
+				packwiz(*flagPackDir, []string{source, "add", mod})
 			}
-			// use a switch statement to determine which command to use
-			switch {
-			case strings.HasPrefix(line, "https://www.curseforge.com/"):
-				installArgs = append(installArgs, "cf", "add", line)
-			case strings.HasPrefix(line, "https://modrinth.com/"):
-				installArgs = append(installArgs, "mr", "add", line)
-			default:
-				installArgs = append(installArgs, "url", "add")
-				filepath := strings.Split(line, "/")       // split the url by /
-				filename := filepath[len(filepath)-1]      // get the last element of the url
-				filename = strings.Split(filename, ".")[0] // remove the file extensions
-				installArgs = append(installArgs, filename)
-				installArgs = append(installArgs, line)
-				// TODO: replace this with importFromStringURL
-				importFromStringURL(line)
+		}
+	} else {
+		for _, mod := range mods[1:] {
+			// get data from parseLine
+			modSplit := strings.Split(mod, ",")
+			url := modSplit[0]
+			metapath := "/"
+			if len(modSplit) > 1 {
+				metapath = modSplit[1]
 			}
-			packwiz(*flagPackDir, installArgs)
+			importFromURL(url, metapath, autoConfirm)
 		}
 	}
+
 }
 
-func importFromStringURL(url string) {
-	fmt.Println("[PackWrap] [NOTICE] ImportFromStringURL functionality will be changing soon")
+func importFromSource(source string, autoConfirm bool) bool {
+	installArgs := []string{}
+	switch {
+	case strings.HasPrefix(source, "https://www.curseforge.com/"):
+		installArgs = append(installArgs, "cf", "add", source)
+	case strings.HasPrefix(source, "https://modrinth.com/"):
+		installArgs = append(installArgs, "mr", "add", source)
+	default:
+		return false
+	}
+	if autoConfirm {
+		installArgs = append(installArgs, "-y")
+	}
+	packwiz(*flagPackDir, installArgs)
+	return true
+}
+
+func importFromURL(url string, targetPath string, autoConfirm bool) bool {
+	installArgs := []string{}
+	if autoConfirm {
+		installArgs = append(installArgs, "-y")
+	}
+
+	installArgs = append(installArgs, "url", "add")
+	// get data from parseLine
+	url, metafolder, filename := parseLine(url, targetPath)
+
+	installArgs = append(installArgs, "--meta-folder", metafolder)
+	if filename == "" {
+		filename = strings.Split(url, "/")[len(strings.Split(url, "/"))-1]
+	} else {
+		installArgs = append(installArgs, "--meta-name", filename)
+	}
+	installArgs = append(installArgs, filename, url)
+	// TODO: manually override the filename in the pw.toml
+
+	packwiz(*flagPackDir, installArgs)
+	return true
 }
